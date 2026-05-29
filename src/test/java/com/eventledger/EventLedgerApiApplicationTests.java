192ce485c293b84bb681a8be98c7827770e47f5c
Siddharth Mane <sidmane20@gmail.com>
2026-05-30 00:30:46 +0530

    chore: scaffold Spring Boot project with H2 and Maven wrapper

---
+++ src/test/java/com/eventledger/EventLedgerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventLedgerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
